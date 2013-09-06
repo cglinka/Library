5a786d1659a4835637beaec79b4135f59e6fb446
Clare Glinka <glinka.cb@gmail.com>
2013-09-06 15:23:15 -0500

    Add working 'check_out' method to Library class for checking out books.

--- library_app.rb
+++ library_app.rb
@@ -1,8 +1,10 @@
 #Create a new book.
 class Book
-  
+  attr_accessor :status, :description
+  attr_reader :author, :title
+
   #initialize variables 'author' and 'title' for each new book.
-  def initialize(author, title)
+  def initialize(title, author)
     @author = author
     @title = title
     @status = "available"
@@ -23,12 +25,34 @@ end
 
 
 class Library
+  attr_reader :books
+
+  def initialize
+    @books = []
+  end
+
+  def add_book(book)
+    @books << book    
+  end
+
+  def list_books
+    
+  end
+
+  def check_out(book, user)
+    book.status = "checked out"
+    user.checked_out_books << book
+  end
+
+  def check_in(book)
+    
+  end
 
 end
 
 #Create user who can check out books.
 class User
-  attr_accessor :@name, :@checked_out_books
+  attr_reader :name, :checked_out_books
 
   def initialize(name)
     @name = name
