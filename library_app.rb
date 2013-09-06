539a7ca329dbd5e164b2dbc203199148458ef620
Clare Glinka <glinka.cb@gmail.com>
2013-09-06 14:05:19 -0500

    Added methods to Book class: initialize(author, title), add_description, get_info

--- library_app.rb
+++ library_app.rb
@@ -1,4 +1,23 @@
+#Create a new book.
 class Book
+  
+  #initialize variables 'author' and 'title' for each new book.
+  def initialize(author, title)
+    @author = author
+    @title = title
+    @status = "available"
+  end
+
+  #optionally add a description to a book that already exists.
+  def add_description(desc)
+    @description = desc
+  end
+
+
+  #Output the title, author, and status of each book.
+  def get_info
+    puts "#{@title} by #{@author} is #{@status}."
+  end
 
 end
 
