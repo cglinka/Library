aaea660460acb3a982fef509860847b0a8b67a33
Clare Glinka <glinka.cb@gmail.com>
2013-09-06 17:07:01 -0500

    Add complexity to Library methods: check_out (checks for number of books the user has checked out, updates book with check out date, adds book to list of checked out books), check_in (checks book status before checking in, updates status, dates, list of checked out books). Added functionality to list_books (lists books in library with status), added working methods list_checked_out, list_overdue (lists checked out and overdue books respectively).

--- library_app.rb
+++ library_app.rb
@@ -1,7 +1,9 @@
+require 'date'
+
 #Create a new book.
 class Book
   attr_accessor :status, :description
-  attr_reader :author, :title
+  attr_reader :author, :title, :check_out_date, :due_date
 
   #initialize variables 'author' and 'title' for each new book.
   def initialize(title, author)
@@ -15,7 +17,6 @@ class Book
     @description = desc
   end
 
-
   #Output the title, author, and status of each book.
   def get_info
     puts "#{@title} by #{@author} is #{@status}."
@@ -24,29 +25,66 @@ class Book
 end
 
 
+# Library Class holds books and methods for updating the status of books.
+
 class Library
+
   attr_reader :books
 
+  # Initialize the three arrays that hold information about books.
   def initialize
     @books = []
+    @checked_out = []
+    @overdue = []
   end
 
+  # Add a book to the library
+  #
+  # book - the Object to be added.
+  #
   def add_book(book)
-    @books << book    
+    if book.is_a?(Book)
+      @books << book
+    end
   end
 
   def list_books
-    
+    @books.each { |book| puts "#{book.title} - #{book.author} : #{book.status}"}
+  end
+
+  def list_checked_out
+    @checked_out.each { |book| puts "#{book.title} - #{book.author}" }
+  end
+
+  def list_overdue
+    @overdue.each { |book| puts "#{book.title} - #{book.author}" }
   end
 
   def check_out(book, user)
-    book.status = "checked out"
-    user.checked_out_books << book
+    if user.checked_out_books.length >= 2
+      puts "You have too many books checked out. You must return at least one book before you can check out another book."      
+    #elsif user.checked_out_books.each {}
+    else 
+      book.status == "available"
+      book.status = "checked out"
+      user.checked_out_books << book
+      book.check_out_date = DateTime.now
+      book.due_date = book.check_out_date + 7
+      @checked_out << book
+    end
   end
 
   def check_in(book, user)
-    book.status = "available"
-    user.checked_out_books.delete_if { |e| e == book }
+    if book.status == "checked out"
+      book.status = "available"
+      user.checked_out_books.delete_if { |e| e == book }
+      book.check_out_date = nil
+      book.due_date = nil
+      @checked_out.delete_if { |e| e == book }
+      @overdue.delete_if { |e| e == book }
+    else
+      puts "This book is not checked out"
+    end
   end
 
 end
